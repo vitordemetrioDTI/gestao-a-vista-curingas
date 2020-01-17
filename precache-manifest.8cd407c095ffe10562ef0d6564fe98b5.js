34ffd443c523d8ebe2aeae479a07402dc9c9dae3
thalissoncorreiadti <correia.tssc@gmail.com>
2020-01-16 21:09:22 -0300

    Updates

--- precache-manifest.8cd407c095ffe10562ef0d6564fe98b5.js
+++ precache-manifest.8cd407c095ffe10562ef0d6564fe98b5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7a5db75fd36325178b19d839f553dbfa",
+    "revision": "eb7743f805c11e810e4dc2b53c1f9872",
     "url": "/gestao-a-vista-curingas/index.html"
   },
   {
-    "revision": "045a56a7e79df8ddb9a3",
+    "revision": "f9ca195820a5ea05383e",
     "url": "/gestao-a-vista-curingas/static/css/main.498d002a.chunk.css"
   },
   {
-    "revision": "063787f887c0f37153a1",
-    "url": "/gestao-a-vista-curingas/static/js/2.27fdc418.chunk.js"
+    "revision": "4a84e248df12f93b934f",
+    "url": "/gestao-a-vista-curingas/static/js/2.c4942287.chunk.js"
   },
   {
-    "revision": "045a56a7e79df8ddb9a3",
-    "url": "/gestao-a-vista-curingas/static/js/main.8a6b374e.chunk.js"
+    "revision": "f9ca195820a5ea05383e",
+    "url": "/gestao-a-vista-curingas/static/js/main.17f3fc19.chunk.js"
   },
   {
     "revision": "0d677622d43ab3f7bfe6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gestao-a-vista-curingas/precache-manifest.d4b95d09b3469b243daddbdac751b08e.js"
+  "/gestao-a-vista-curingas/precache-manifest.8cd407c095ffe10562ef0d6564fe98b5.js"
 );
 
 self.addEventListener('message', (event) => {
