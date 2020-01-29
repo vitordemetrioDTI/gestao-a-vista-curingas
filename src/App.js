cdb7b757d236b9757abeb6f88d130d305b8f1d9e
Matheus Becatini <60360393+matheus-becatini@users.noreply.github.com>
2020-01-29 17:17:44 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -86,8 +86,8 @@ class App extends React.Component {
           {this.state.play ? (
             <PauseButton style={{ color: '#ffffff' }} />
           ) : (
-            <PlayButton style={{ color: '#ffffff' }} />
-          )}
+              <PlayButton style={{ color: '#ffffff' }} />
+            )}
         </Fab>
       </MuiThemeProvider>
     );
