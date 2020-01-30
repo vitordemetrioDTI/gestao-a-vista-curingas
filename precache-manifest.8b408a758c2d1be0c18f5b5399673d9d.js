228c31d894f3cefb3630a90f728898fd7b2ed10d
Matheus Becatini <60360393+matheus-becatini@users.noreply.github.com>
2020-01-30 11:09:40 -0300

    Updates

---
+++ precache-manifest.8b408a758c2d1be0c18f5b5399673d9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78658d721d5777b19d9665e14d34a75b",
+    "url": "/gestao-a-vista-curingas/index.html"
+  },
+  {
+    "revision": "cc5164ae6246819ee739",
+    "url": "/gestao-a-vista-curingas/static/css/main.1c0a5e17.chunk.css"
+  },
+  {
+    "revision": "5f33a7035449a596864d",
+    "url": "/gestao-a-vista-curingas/static/js/2.f233f5b1.chunk.js"
+  },
+  {
+    "revision": "7e7a577e431a3cfdd544d84f034d58c8",
+    "url": "/gestao-a-vista-curingas/static/js/2.f233f5b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc5164ae6246819ee739",
+    "url": "/gestao-a-vista-curingas/static/js/main.d03f2951.chunk.js"
+  },
+  {
+    "revision": "ad9598cf00ddaf0ddb89",
+    "url": "/gestao-a-vista-curingas/static/js/runtime-main.6ed5ea8f.js"
+  }
+]);
