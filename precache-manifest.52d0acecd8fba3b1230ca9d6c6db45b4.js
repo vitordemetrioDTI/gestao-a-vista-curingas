8941a462506e85a6137f978542df93458dbf219b
thalissoncorreiadti <correia.tssc@gmail.com>
2020-01-31 16:37:33 -0300

    Updates

---
+++ precache-manifest.52d0acecd8fba3b1230ca9d6c6db45b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36217054f9cc59025f17641a4e8e891",
+    "url": "/gestao-a-vista-curingas/index.html"
+  },
+  {
+    "revision": "e4bb86ee45bc0eb3f259",
+    "url": "/gestao-a-vista-curingas/static/css/main.1c0a5e17.chunk.css"
+  },
+  {
+    "revision": "db7fbd50de1b87755cc4",
+    "url": "/gestao-a-vista-curingas/static/js/2.4c31700b.chunk.js"
+  },
+  {
+    "revision": "e8d177551df72b2c6c3a2a53c7158b53",
+    "url": "/gestao-a-vista-curingas/static/js/2.4c31700b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4bb86ee45bc0eb3f259",
+    "url": "/gestao-a-vista-curingas/static/js/main.05ead183.chunk.js"
+  },
+  {
+    "revision": "ad9598cf00ddaf0ddb89",
+    "url": "/gestao-a-vista-curingas/static/js/runtime-main.6ed5ea8f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gestao-a-vista-curingas/precache-manifest.581d645b4ea31ddaefb8fc063183592d.js"
+  "/gestao-a-vista-curingas/precache-manifest.52d0acecd8fba3b1230ca9d6c6db45b4.js"
 );
 
 self.addEventListener('message', (event) => {
