42cac6746ade38403f8b5903c997006b0c1c1d9e
thalissoncorreiadti <correia.tssc@gmail.com>
2020-02-03 17:20:05 -0300

    Updates

---
+++ precache-manifest.2d07f27e1b53d6d7af7f47e6839728bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a949c06b015ed7ff194cf135804bf9",
+    "url": "/gestao-a-vista-curingas/index.html"
+  },
+  {
+    "revision": "695318c487a1ccfd788b",
+    "url": "/gestao-a-vista-curingas/static/css/main.1c0a5e17.chunk.css"
+  },
+  {
+    "revision": "39f6cfb2320e4c60b107",
+    "url": "/gestao-a-vista-curingas/static/js/2.df740361.chunk.js"
+  },
+  {
+    "revision": "e8d177551df72b2c6c3a2a53c7158b53",
+    "url": "/gestao-a-vista-curingas/static/js/2.df740361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695318c487a1ccfd788b",
+    "url": "/gestao-a-vista-curingas/static/js/main.76aa88ed.chunk.js"
+  },
+  {
+    "revision": "ad9598cf00ddaf0ddb89",
+    "url": "/gestao-a-vista-curingas/static/js/runtime-main.6ed5ea8f.js"
+  }
+]);
