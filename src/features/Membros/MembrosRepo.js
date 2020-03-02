15471c1ea338b95276c175b1c8f65238a97578c6
Matheus Becatini <60360393+matheus-becatini@users.noreply.github.com>
2020-03-02 11:53:51 -0300

    Repositorios originais

--- src/features/Membros/MembrosRepo.js
+++ src/features/Membros/MembrosRepo.js
@@ -4,7 +4,7 @@ class MembrosRepo {
   async listarMembros() {
     try {
       const tsvMembros = await axios.get(
-        `https://docs.google.com/spreadsheets/d/e/2PACX-1vT-B4xEWT8OUz-Vuk53ofBxpglD0Qh_Rik3uZId3Uz-7Z_qjJfGgdNOmWp69-u0sDHAoFDcFe_48K3k/pub?gid=1653355093&single=true&output=tsv`
+        `https://docs.google.com/spreadsheets/d/e/2PACX-1vSpC_x6FOQi7QOG4-gFFZzgIp_BCHZEKLHy8PoJpA9twqOAsRVMUerK9BEhglaI92K58qjW4DOFFkMC/pub?gid=1877599560&single=true&output=tsv`
       );
       return tsvMembros;
     } catch (erro) {
