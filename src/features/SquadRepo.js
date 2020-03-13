fe23b03f290c074cb05b6ef7d136d70073e23ce9
Matheus Becatini <60360393+matheus-becatini@users.noreply.github.com>
2020-03-13 15:59:40 -0300

    Repo fix

--- src/features/SquadRepo.js
+++ src/features/SquadRepo.js
@@ -8,7 +8,7 @@ class SquadRepo {
     const options = {
       method: "GET",
       url:
-        "https://docs.google.com/spreadsheets/d/e/2PACX-1vSpC_x6FOQi7QOG4-gFFZzgIp_BCHZEKLHy8PoJpA9twqOAsRVMUerK9BEhglaI92K58qjW4DOFFkMC/pub?gid=766563780&single=true&output=tsv",
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vSpC_x6FOQi7QOG4-gFFZzgIp_BCHZEKLHy8PoJpA9twqOAsRVMUerK9BEhglaI92K58qjW4DOFFkMC/pub?gid=546432117&single=true&output=tsv",
       headers: {}
     };
     try {
